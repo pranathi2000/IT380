7c5b6f1ee02087ec719e9268c65ea1b254a63de6
Pranathi Pothukanuri <pranathi2000@gmail.com>
2020-09-26 21:36:42 -0400

    Created Title Menu and 3 Buttons

---
+++ Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Hello World! This is my first script");
+    }
+
+    public void SetScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
